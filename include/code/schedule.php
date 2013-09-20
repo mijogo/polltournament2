701947aac381cb13b67a0c1759ad5ec3ee7e149f
Michel Jofre <michel.jofreg@gmail.com>
2013-09-20 20:11:46 -0300

    se agrego una forma de sorteo

--- include/code/schedule.php
+++ include/code/schedule.php
@@ -35,6 +35,19 @@ class Schedule
 				{
 					$this->changeChampionship($process[$i]->gettargetint());
 				}
+				if($process[$i]->getaccion()=="CHEVE")
+				{
+					$this->changeEvento($process[$i]->gettargetint());
+				}
+				if($process[$i]->getaccion()=="CALPO")
+				{
+					$this->calcularPonderacion();
+				}
+				
+				if($process[$i]->getaccion()=="INMAT")
+				{
+					$this->ingresarMatch();
+				}			
 				$process[$i]->setHecho(1);
 				$process[$i]->update(1,array("hecho"),1,array("id"));
 				$lognew = new log(-10,$process[$i]->getaccion(),$fechaActual,1,"system",$process[$i]->gettargetstring()." ".$process[$i]->gettargetdate()." ".$process[$i]->gettargetint());
@@ -44,13 +57,144 @@ class Schedule
 				$sigue=false;
 		}
 	}
-	function sorteo($instancia="",$numeroGrupo="",$tipo="",$seed="")
-	{}
+	function sorteo($instancia="",$numeroGrupo="",$tipo=0,$seed=0)
+	{
+		if($tipo == "ELIMI")
+		{		
+			$torneoActual = new torneo();
+			$torneoActual->setactivo(1);
+			$torneoActual = $torneoActual->read(false,1,array("activo"));
+			
+			$newConf = new configuracion();
+			$newConf->setnombre($instancia);
+			$newConf->setidtorneo($torneoActual->getid());
+			$newConf = $newConf->read(false,2,array("nombre","idtorneo"));
+			
+			$personajeTotal = new personajepar();
+			$personajeTotal->setronda($instancia);
+			$personajeTotal = $personajeTotal->read(true,1,array("ronda"));
+			$numeroTotal = count($personajeTotal);
+			$cantidadGrupo = floor($numeroTotal/$newConf->getnumerogrupos());
+			if(($personajeTotal-($cantidadGrupo*$newConf->getnumerogrupos()))>=$numeroGrupo)
+				$cantidadGrupo++;
+				
+			$perListos = new personajepar()
+			$perListos->setronda($instancia);
+			$perListos->setgrupo($numeroGrupo);
+			$perListos->setidtorneo($torneoActual->getid());
+			$perListos = $perListos->read(true,3,array("ronda","AND","grupo","AND","idtorneo"));
+			$cantidadListos = count($perListos);
+			
+			$personajesSortear = new personajepar();
+			$personajesSortear->setronda($instancia);
+			$personajesSortear->setgrupo("N");
+			$personajesSortear->setidtorneo($torneoActual->getid());
+			$personajesSortear = $personajesSortear->read(true,3,array("ronda","AND","grupo","AND","idtorneo"));
+			
+			for($i=0;$i<count($personajesSortear);$i++)
+			{
+				$perSort[$i]["prox"]=($i+1)%count($personajesSortear);
+				$perSort[$i]["ant"]=($i-1)%count($personajesSortear);
+				$perSort[$i]["act"]=0;
+			}
+			if($seed==1)
+			{
+				$totalPonderacion = 0;
+				for($i=0;$i<$cantidadListos;$i++)
+					$totalPonderacion += $perListos[$i]->getponderacion();
+				
+				for($i=$cantidadListos;$i<$cantidadGrupo;$i++)
+				{
+					$escoger=rand(0,count($personajesSortear));
+					$sigue=true;
+					while($sigue)
+					{
+						if($perSort[$escoger]["act"]==1)
+							$escoger=$perSort[$escoger]["prox"];
+						else
+						{
+							$porcentaje = ($totalPonderacion+$personajesSortear[$escoger]->getponderacion())/($cantidadListos+$i+1);
+							$porcentaje = abs(($porcentaje-$torneoActual->getponderacionprom())/$torneoActual->getponderacionprom())
+							$porcentaje = 1000 - $porcentaje*1000;
+							if($porcentaje<0)
+								$escoger=$perSort[$escoger]["prox"];
+							else
+							{
+								$res = rand(0,1000);
+								if($res<$porcentaje)
+								{
+									$personajesSortear[$escoger]->setgrupo($numeroGrupo);
+									$personajesSortear[$escoger]->update(1,array("grupo"),1,array("id"))
+									$perSort[$escoger]["act"]=1;
+									$temSort = $perSort[$escoger];
+									
+									$tempProx=$perSort[$escoger]["prox"];
+									while($perSort[$tempProx]["act"]==1)
+										$tempProx=$perSort[$tempMov]["prox"]
+									$tempAnt=$perSort[$escoger]["ant"];
+									while($perSort[$tempAnt]["act"]==1)
+										$tempAnt=$perSort[$tempAnt]["ant"]
+										
+									$perSort[$escoger]["prox"]=$tempProx;
+									$perSort[$escoger]["ant"]=$tempAnt;
+									$perSort[$tempAnt]["prox"]=$tempProx;
+									$perSort[$tempProx]["ant"]=$tempAnt;
+									$sigue=false;
+									
+									$totalPonderacion += $personajesSortear[$escoger]->getponderacion();							
+								}
+								else
+									$escoger=$perSort[$escoger]["prox"];
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				for($i=$cantidadListos;$i<$cantidadGrupo;$i++)
+				{
+					$escoger=rand(0,count($personajesSortear));
+					$sigue=true;
+					while($sigue)
+					{
+						if($perSort[$escoger]["act"]==1)
+							$escoger=$perSort[$escoger]["prox"];
+						else
+						{
+							$personajesSortear[$escoger]->setgrupo($numeroGrupo);
+							$personajesSortear[$escoger]->update(1,array("grupo"),1,array("id"))
+							$perSort[$escoger]["act"]=1;
+							$temSort = $perSort[$escoger];
+									
+							$tempProx=$perSort[$escoger]["prox"];
+							while($perSort[$tempProx]["act"]==1)
+								$tempProx=$perSort[$tempMov]["prox"]
+							$tempAnt=$perSort[$escoger]["ant"];
+							while($perSort[$tempAnt]["act"]==1)
+								$tempAnt=$perSort[$tempAnt]["ant"]
+										
+							$perSort[$escoger]["prox"]=$tempProx;
+							$perSort[$escoger]["ant"]=$tempAnt;
+							$perSort[$tempAnt]["prox"]=$tempProx;
+							$perSort[$tempProx]["ant"]=$tempAnt;
+							$sigue=false;
+						}
+					}
+				}
+			}
+		}
+	}
 	function activarBatalla($fecha="")
 	{}
 	function ConteoVotos()
 	{}
 	function changeChampionship($nuevoEstado="")
 	{}
+	function changeEvento($nuevoEstado="")
+	{}
+	function calcularPonderacion()
+	{}
+
 }
 ?>
