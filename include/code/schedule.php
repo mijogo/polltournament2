660066a7720fde2635771be3b08b537c4483c6da
Michel Jofre <michel.jofreg@gmail.com>
2013-09-23 23:19:49 -0300

    se avanza con los sorteos

--- include/code/schedule.php
+++ include/code/schedule.php
@@ -129,11 +129,11 @@ class Schedule
 									$temSort = $perSort[$escoger];
 									
 									$tempProx=$perSort[$escoger]["prox"];
-									while($perSort[$tempProx]["act"]==1)
-										$tempProx=$perSort[$tempMov]["prox"]
+									while($perSort[$tempProx]["act"]==1&&$perSort[$tempMov]["prox"]!=$escoger)
+										$tempProx=$perSort[$tempMov]["prox"];
 									$tempAnt=$perSort[$escoger]["ant"];
-									while($perSort[$tempAnt]["act"]==1)
-										$tempAnt=$perSort[$tempAnt]["ant"]
+									while($perSort[$tempAnt]["act"]==1&&$perSort[$tempMov]["ant"]!=$escoger)
+										$tempAnt=$perSort[$tempAnt]["ant"];
 										
 									$perSort[$escoger]["prox"]=$tempProx;
 									$perSort[$escoger]["ant"]=$tempAnt;
@@ -181,6 +181,110 @@ class Schedule
 							$sigue=false;
 						}
 					}
+				}
+			}
+		}
+		elseif($tipo == "ELGRU")
+		{
+			$torneoActual = new torneo();
+			$torneoActual->setactivo(1);
+			$torneoActual = $torneoActual->read(false,1,array("activo"));
+			
+			$newConf = new configuracion();
+			$newConf->setnombre($instancia);
+			$newConf->setidtorneo($torneoActual->getid());
+			$newConf = $newConf->read(false,2,array("nombre","idtorneo"));
+			
+			$personajeTotal = new personajepar();
+			$personajeTotal->setronda($instancia);
+			$personajeTotal = $personajeTotal->read(true,1,array("ronda"));
+			$numeroTotal = count($personajeTotal);
+			
+			$totalDelGrupo = 0;
+			$cantidadListos = 0;
+			$totalPonderacion= 0;
+			for($i=0;$i<$newConf->getnumerobatallas();$i++)
+			{
+				$datosGrupo[$i]["cantidad"] = floor($numeroTotal/($newConf->getnumerogrupos()*$newConf->getnumerobatallas()));
+				$datosGrupo[$i]["nombre"] = $numeroGrupo."-".($i+1);
+				if(($personajeTotal-($cantidadGrupo*$newConf->getnumerogrupos()))>=convertNumber($numeroGrupo)+$i*$newConf->getnumerogrupos())
+					$datosGrupo[$i]["cantidad"]++;
+				$totalDelGrupo += $datosGrupo[$i]["cantidad"];
+				
+				$perListos = new personajepar()
+				$perListos->setronda($instancia);
+				$perListos->setgrupo($datosGrupo[$i]["nombre"]);
+				$perListos->setidtorneo($torneoActual->getid());
+				$perListos = $perListos->read(true,3,array("ronda","AND","grupo","AND","idtorneo"));
+				$cantidadListos += count($perListos);
+				if(count($perListos)>0&&$seed==1)
+					for($j=0;$j<count($perListos);$j++)
+						$totalPonderacion+=$perListos[$j]->getponderacion();
+			}
+			$personajesSortear = new personajepar();
+			$personajesSortear->setronda($instancia);
+			$personajesSortear->setgrupo("N");
+			$personajesSortear->setidtorneo($torneoActual->getid());
+			$personajesSortear = $personajesSortear->read(true,3,array("ronda","AND","grupo","AND","idtorneo"));
+			for($i=0;$i<count($personajesSortear);$i++)
+			{
+				$perSort[$i]["prox"]=($i+1)%count($personajesSortear);
+				$perSort[$i]["ant"]=($i-1)%count($personajesSortear);
+				$perSort[$i]["act"]=0;
+			}
+			if($seed==1)
+			{
+				for($i=$cantidadListos;$i<$totalDelGrupo;$i++)
+				{
+							
+					$escoger=rand(0,count($personajesSortear));
+					$sigue=true;
+					while($sigue)
+					{
+						if($perSort[$escoger]["act"]==1)
+							$escoger=$perSort[$escoger]["prox"];
+						else
+						{
+							$porcentaje = ($totalPonderacion+$personajesSortear[$escoger]->getponderacion())/($cantidadListos+$i+1);
+							$porcentaje = abs(($porcentaje-$torneoActual->getponderacionprom())/$torneoActual->getponderacionprom())
+							$porcentaje = 1000 - $porcentaje*1000;
+							if($porcentaje<15)
+								$porcentaje=15;
+							$res = rand(0,1000);
+							if($res<$porcentaje)
+							{
+								$sorGrupo[$i-$cantidadListos]["prox"]=($i-$cantidadListos+1)%(count($totalDelGrupo)-$cantidadListos);
+								$sorGrupo[$i-$cantidadListos]["ant"]=($i-$cantidadListos-1)%(count($totalDelGrupo)-$cantidadListos);
+								$sorGrupo[$i-$cantidadListos]["act"]=0;
+								$sorGrupo[$i-$cantidadListos]["id"]=$escoger;
+
+								$perSort[$escoger]["act"]=1;
+								$temSort = $perSort[$escoger];
+											
+								$tempProx=$perSort[$escoger]["prox"];
+								while($perSort[$tempProx]["act"]==1&&$perSort[$tempMov]["prox"]!=$escoger)
+									$tempProx=$perSort[$tempMov]["prox"];
+								$tempAnt=$perSort[$escoger]["ant"];
+								while($perSort[$tempAnt]["act"]==1&&$perSort[$tempMov]["ant"]!=$escoger)
+									$tempAnt=$perSort[$tempAnt]["ant"];
+												
+								$perSort[$escoger]["prox"]=$tempProx;
+								$perSort[$escoger]["ant"]=$tempAnt;
+								$perSort[$tempAnt]["prox"]=$tempProx;
+								$perSort[$tempProx]["ant"]=$tempAnt;
+								$sigue=false;
+											
+								$totalPonderacion += $personajesSortear[$escoger]->getponderacion();							
+							}
+							else
+								$escoger=$perSort[$escoger]["prox"];
+						}
+					}
+				}
+				$datosGrupo[$i]["cantidad"]
+				for($i=0;$i<count($datosGrupo);$i++)
+				{
+					
 				}
 			}
 		}
